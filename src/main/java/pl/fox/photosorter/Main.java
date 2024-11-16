7c3a9904d39b10423ad70ac8c9986907d909505e
bchotkowski <bm.chotkowski@gmail.com>
2024-11-16 16:44:06 +0100

    Tried to configure args, will have to work on it

--- src/main/java/pl/fox/photosorter/Main.java
+++ src/main/java/pl/fox/photosorter/Main.java
@@ -1,8 +1,18 @@
 package pl.fox.photosorter;
 
+import pl.fox.photosorter.utils.PropertySource;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+//        if (args != null && args.length > 0) {
+//            System.out.println(Arrays.toString(args));
+//            PropertySource.overrideProperty("inputPath", args[0]);
+//        }
+
+        PropertySource.printProperties();
         new Processor().run();
     }
 
